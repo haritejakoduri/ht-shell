da9399d47146793aefec4cb50d4d9ed188271a68
haritejakoduri <haritejakoduri1234@gmail.com>
2021-08-29 23:22:17 +0530

    argument support added

--- index.js
+++ index.js
@@ -14,12 +14,18 @@ try {
    if(term1==='help'){
       console.log(json)
    }
-   else if(term1!='' && json[`${term1}`].exe!=undefined){
-      if(term1=="exit") 
-      {
-         loop=false
-         return;
-      }
+   else if(term1==="exit") 
+   {
+      loop=false
+   }
+   else if(term1!=''&& term1.split(' ').length!=0 &&  json[`${term1.split(' ')[0]}`].hasArguments && json[`${term1.split(' ')[0]}`].exe!=undefined){
+      term1=term1.split(' ')
+      let cmd=term1[0];
+      term1.shift()
+      term1=term1.join(' ')
+      c.command(`${json[`${cmd}`].exe} ${term1}`)
+   }
+   else if(term1!='' && json[`${term1}`].exe!=undefined){     
       c.command(`${json[`${term1}`].exe}`)
    }
 } catch (error) {
